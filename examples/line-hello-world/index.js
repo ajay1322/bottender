1e4ab2ae738daffd478a0099158661cf47750f64
ajay1322 <52128399+ajay1322@users.noreply.github.com>
2019-06-23 17:46:43 +0530

    Update index.js

--- examples/line-hello-world/index.js
+++ examples/line-hello-world/index.js
@@ -2,8 +2,8 @@ const { LineBot } = require('bottender');
 const { createServer } = require('bottender/express');
 
 const bot = new LineBot({
-  channelSecret: '__FILL_YOUR_SECRET_HERE__',
-  accessToken: '__FILL_YOUR_TOKEN_HERE__',
+  channelSecret: 'c1843fd0381fb0fa8c64035fb6d3470e',
+  accessToken: '7sYKfRYhRZNsJoK3Uwxjx0DlAvssKg+O9I9Wy5y+nOF+nwclOY1Alg50iZh4m1QoQkzQuKFY4vRFKCKY8S7mbfeuX7p+Y/uzuPWb4Ws4IY8Se90YI51w42iVp+8VCLzBdH05mHFhnLwVz2S+GfPrfAdB04t89/1O/w1cDnyilFU=',
 });
 
 bot.onEvent(async context => {
